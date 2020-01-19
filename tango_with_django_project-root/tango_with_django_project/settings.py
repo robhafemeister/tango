28c042b990088620c636dd1a6afa7b995995b242
robhafemeister <robhafemeister@gmail.com>
2020-01-19 07:58:59 -0500

    Added static resources

--- tango_with_django_project-root/tango_with_django_project/settings.py
+++ tango_with_django_project-root/tango_with_django_project/settings.py
@@ -14,6 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR,]
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+# Print project path information.
+# print(__file__)
+# print("-" * 10)
+# print(os.path.dirname(__file__))
+# print("*" * 10)
+# print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# print("#" * 10)
